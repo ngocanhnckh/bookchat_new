304f1f6a6783a9c5511c97d2dfc2d441b74be61c
Mufaddal Hamid <mufaddalhamidofficial@gmail.com>
2021-12-21 21:35:53 +0530

    21-12

--- lib/screens/home/home_tabs.dart
+++ lib/screens/home/home_tabs.dart
@@ -3,6 +3,7 @@ import 'package:book_chat/screens/home/library.dart';
 import 'package:book_chat/screens/home/menu.dart';
 import 'package:book_chat/screens/home/notification.dart';
 import 'package:book_chat/screens/home/web.dart';
+import 'package:book_chat/utils/utils.dart';
 import 'package:flutter/material.dart' hide Notification;
 
 import '../../../utils/icon_image.dart';
@@ -11,9 +12,7 @@ import '../../widgets/app_scaffold.dart';
 class HomeTabBarWrapper extends StatelessWidget {
   static const String routeName = '/home';
 
-  HomeTabBarWrapper({
-    final Key? key,
-  }) : super(key: key);
+  HomeTabBarWrapper({final Key? key}) : super(key: key);
 
   final ValueNotifier<int> currentPage = ValueNotifier<int>(0);
   final PageController pageController = PageController();
@@ -26,44 +25,69 @@ class HomeTabBarWrapper extends StatelessWidget {
   @override
   Widget build(final BuildContext context) {
     final MediaQueryData mediaQuery = MediaQuery.of(context);
-
     return AppScaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Stack(
+          Container(
+            color: Colors.white,
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
             alignment: Alignment.center,
-            children: <Widget>[
-              Container(
-                height: mediaQuery.size.height,
-                width: mediaQuery.size.width,
-                padding: EdgeInsets.only(bottom: 72 + mediaQuery.viewPadding.bottom),
-                color: const Color(0xFFF9F9F9),
-                child: PageView(
-                  onPageChanged: (final int page) => currentPage.value = page,
-                  controller: pageController,
-                  children: const <Widget>[Home(), Web(), Library(), Notification(), Menu()],
-                ),
+            child: SafeArea(
+              child: Row(
+                children: [
+                  Image.asset('lib/assets/images/logo.png', height: 35),
+                  const Spacer(),
+                  if (isBigScreen(context)) tabs(),
+                  const Spacer(),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.chat_bubble_outline)),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(100)),
+                    ),
+                  ),
+                ],
               ),
-              Positioned(
-                bottom: 0,
-                width: mediaQuery.size.width,
-                height: 72 + mediaQuery.viewPadding.bottom,
-                child: ValueListenableBuilder<int>(
-                  valueListenable: currentPage,
-                  builder: (final __, final int page, final _) => Row(
-                    children: <Widget>[
-                      Expanded(child: BottomBarIcon(onTap: () => changePage(0), selected: page == 0, image: getIconImage('home'), isFirst: true)),
-                      Expanded(child: BottomBarIcon(onTap: () => changePage(1), selected: page == 1, image: getIconImage('language'))),
-                      Expanded(child: BottomBarIcon(onTap: () => changePage(2), selected: page == 2, image: getIconImage('bookmark'))),
-                      Expanded(child: BottomBarIcon(onTap: () => changePage(3), selected: page == 3, image: getIconImage('notification'))),
-                      Expanded(child: BottomBarIcon(onTap: () => changePage(4), selected: page == 4, image: getIconImage('menu'), isLast: true)),
-                    ],
+            ),
+          ),
+          Expanded(
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                Container(
+                  width: mediaQuery.size.width,
+                  padding: EdgeInsets.only(bottom: 72 + mediaQuery.viewPadding.bottom),
+                  color: const Color(0xFFF9F9F9),
+                  child: PageView(
+                    onPageChanged: (final int page) => currentPage.value = page,
+                    controller: pageController,
+                    children: const <Widget>[Home(), Web(), Library(), Notification(), Menu()],
                   ),
                 ),
-              ),
-            ],
+                if (!isBigScreen(context))
+                  Positioned(bottom: 0, width: mediaQuery.size.width, height: 72 + mediaQuery.viewPadding.bottom, child: tabs()),
+              ],
+            ),
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget tabs() {
+    return ValueListenableBuilder<int>(
+      valueListenable: currentPage,
+      builder: (final __, final int page, final _) => Row(
+        children: <Widget>[
+          Expanded(child: BottomBarIcon(onTap: () => changePage(0), selected: page == 0, image: getIconImage('home'), isFirst: true)),
+          Expanded(child: BottomBarIcon(onTap: () => changePage(1), selected: page == 1, image: getIconImage('language'))),
+          Expanded(child: BottomBarIcon(onTap: () => changePage(2), selected: page == 2, image: getIconImage('bookmark'))),
+          Expanded(child: BottomBarIcon(onTap: () => changePage(3), selected: page == 3, image: getIconImage('notification'))),
+          Expanded(child: BottomBarIcon(onTap: () => changePage(4), selected: page == 4, image: getIconImage('menu'), isLast: true)),
         ],
       ),
     );
@@ -92,8 +116,10 @@ class BottomBarIcon extends StatelessWidget {
   Widget build(final BuildContext context) => Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.only(
-            topLeft: isFirst ? const Radius.circular(16) : Radius.zero,
-            topRight: isLast ? const Radius.circular(16) : Radius.zero,
+            topLeft: isFirst && !isBigScreen(context) ? const Radius.circular(16) : Radius.zero,
+            topRight: isLast && !isBigScreen(context) ? const Radius.circular(16) : Radius.zero,
+            bottomLeft: isFirst && isBigScreen(context) ? const Radius.circular(16) : Radius.zero,
+            bottomRight: isLast && isBigScreen(context) ? const Radius.circular(16) : Radius.zero,
           ),
           color: selected ? Colors.white : Theme.of(context).primaryColor,
         ),
